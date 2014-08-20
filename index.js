f95d07e3315a65c15736b0866cee0adf141a7bbf
ksimanenka <simatyt@gmail.com>
2014-08-20 14:58:32 +0300

    save

--- index.js
+++ index.js
@@ -23,17 +23,14 @@ var sendFsData = function(res) {
 
 var handler = function(req, res) {
 	console.log("Path from request : " + url.parse(req.url).pathname);
-	console.log("Decoded from request : " + decodeURIComponent(url.parse(req.url).pathname));
-	var targetPath = path.normalize(rootDir + "/" + url.parse(req.url).pathname);
-	console.log("Path to search : " + decodeURIComponent(targetPath));
+	var targetPath = decodeURIComponent(path.normalize(rootDir + "/" + url.parse(req.url).pathname));
+	console.log("Path to search : " + targetPath);
 	fs.exists( targetPath, function (isExist) {
 		if( isExist ) {
 			var stat = fs.lstatSync(targetPath);
 			if(stat.isDirectory()) {
 				fs.readdir( targetPath, sendFsData(res));
 			} else if (stat.isFile()) {
-				console.log("ddddd : " + ext.getExt(targetPath));
-				console.log("aaaaa : " + ext.getContentType(ext.getExt(targetPath)));
 				res.setHeader("Content-Type", ext.getContentType(ext.getExt(targetPath)));
 				fs.readFile(targetPath, 'utf8',sendFsData(res));
 			}
