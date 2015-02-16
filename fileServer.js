d1e55dafcab33de277c96b5178bad765612f0ead
Fancy Coder <hatalliam@gmail.com>
2015-02-16 19:15:27 +0300

    Fixing issues marked by trainer
    - Fixing 404 status code returned
    - Using "Q" module to make code more flatten
    - Exporting function instead of creating a class.

--- fileServer.js
+++ fileServer.js
@@ -4,15 +4,13 @@ var url = require("url");
 var fileStr = require("fs");
 var m_http = require("http");
 var mime = require("mime")
-
-var FileServer = function() {
-
-};
+var async = require("async");
+var Q = require("q");
 
 //Creates static file server listed to the specified port.
-FileServer.prototype.createServer = function(port) {
+exports.createServer = function(port) {
   
-	m_http.createServer(function(request, response)
+    m_http.createServer(function(request, response)
 	{
 		var m_path = unescape(url.parse(request.url).pathname);
 
@@ -26,39 +24,38 @@ FileServer.prototype.createServer = function(port) {
 					response.end();
 				}
 				else 
-				{
-					fileStr.lstat(m_fullPath, function(err, stats){
-					if (stats.isFile()) {
+				{					
+					Q.nfcall(fileStr.lstat, m_fullPath)
+					.then(function(stats){
+						if (stats.isFile()) {
 
-						console.log("%s is a file, file server may proceed.", m_path);
+							console.log("%s is a file, file server may proceed.", m_path);
 
-				    	var m_mime = mime.lookup(m_fullPath);
-						console.log("Mime for %s is %s", m_path, m_mime);
+					    	var m_mime = mime.lookup(m_fullPath);
+							console.log("Mime for %s is %s", m_path, m_mime);
 
-						var readFile = fileStr.createReadStream(m_fullPath);
-			  			response.writeHeader(200, {"Content-Type":m_mime});
-						readFile.pipe(response);
-				    } 
-				    else if (stats.isDirectory()) 
-				    {
-				    	console.log("%s is a directory!", m_path);
-					    response.writeHead(200, {"Content-Type":"text/plain"});
-					    response.write("Please specify correct the file path.");
-					    response.end();
-					} 
-					else 
-					{
-					    response.writeHead(500, {"Content-Type": "text/plain"});
-					    response.write("Internal server error!");
-					    response.end();
-					}
-				}); 	
+							var readFile = fileStr.createReadStream(m_fullPath);
+				  			response.writeHeader(200, {"Content-Type":m_mime});
+							readFile.pipe(response);
+					    } 
+					    else if (stats.isDirectory()) 
+					    {
+					    	console.log("%s is a directory!", m_path);
+						    response.writeHead(404, {"Content-Type":"text/plain"});
+						    response.write("Please specify correct the file path.");
+						    response.end();
+						} 
+						else 
+						{
+						    response.writeHead(500, {"Content-Type": "text/plain"});
+						    response.write("Internal server error!");
+						    response.end();
+						}		
+					});
 			}
 		});
 	}).listen(port);
 
 	console.log("File Server is running on %s port!", port);
 };
-
-module.exports = FileServer;
 
